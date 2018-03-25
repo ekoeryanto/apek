aa5a07aac8e8fe12c7798686b87ed2b1034a9181
Eko Eryanto <ekoeryanto@gmail.com>
2018-03-25 22:10:55 +0700

    chore: add pages

---
+++ pages/secretariat.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="container">
+    <h2>{{ title }}</h2>
+    <div v-html="$md.render(body)"/>
+  </section>
+</template>
+
+<script>
+export default {
+  head: () => ({
+    title: 'Sekretariat',
+  }),
+  async asyncData() {
+    const data = await import('~/content/pages/secretariat.json');
+    return data;
+  },
+};
+</script>
