6c80862a60884c83ef0a1521c7997eb45bed10b0
Eko Eryanto <ekoeryanto@gmail.com>
2018-03-27 11:53:49 +0700

    chore: add progressive image component

---
+++ components/image.vue
@@ -0,0 +1,125 @@
+<template>
+  <div
+    ref="image"
+    class="progressive-image">
+    <canvas
+      v-if="!shouldImageRender"
+      ref="canvas"
+      width="1"
+      height="1"
+      class="canvas"
+    />
+    <div
+      :style="wrapperStyle"
+      class="progressive-image-wrapper"
+    >
+      <transition
+        enter-class="progressive-image-enter"
+        enter-active-class="progressive-image-before-enter">
+        <img
+          v-show="shouldImageRender"
+          ref="main"
+          :src="image"
+          :alt="alt"
+          class="progressive-image-main"
+        >
+      </transition>
+      <transition
+        enter-class="progressive-image-enter"
+        enter-active-class="progressive-image-before-enter">
+        <div
+          v-if="shouldPlaceholderRender"
+          :class="{ 'progressive-image-placeholder-out': shouldImageRender }"
+          :style="placeholderStyle"
+          class="progressive-image-placeholder"
+        />
+      </transition>
+    </div>
+  </div>
+</template>
+
+<script>
+import image from '@/mixins/image';
+
+export default {
+  name: 'ProgressiveImg',
+
+  mixins: [image],
+
+  computed: {
+    placeholderStyle() {
+      return {
+        ...this.blurStyle,
+        'background-image': `url(${this.placeholder ||
+          this.options.placeholder})`,
+      };
+    },
+  },
+};
+</script>
+
+<style lang="css">
+.progressive-image {
+  position: relative;
+  overflow: hidden;
+}
+
+.canvas {
+  visibility: hidden;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+
+.progressive-image-main {
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: auto;
+  z-index: 1;
+  transition-duration: 0.3s;
+  transition-property: all;
+  transition-timing-function: ease-out;
+  transform: translateZ(0);
+}
+
+.progressive-image-before-enter {
+  opacity: 1;
+}
+
+.progressive-image-enter {
+  opacity: 0;
+}
+
+.progressive-image-placeholder {
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  z-index: 0;
+  overflow: hidden;
+  transition-duration: 300ms;
+  transition-property: all;
+  transition-timing-function: ease-out;
+  backface-visibility: hidden;
+  transform: translateZ(0) scale(1.1);
+  width: 100%;
+  height: 100%;
+  background-size: cover;
+}
+
+.progressive-image-placeholder-out {
+  transition-duration: inherit;
+  transition-property: all;
+  transition-timing-function: ease-out;
+
+  /**
+     * the transitioon delay needs to be longer than the
+     * .progressive-image-main transition-duration, otherwise it will flick
+     * because there won't be a background.
+    */
+  transition-delay: 0.4s;
+
+  opacity: 0;
+}
+</style>
