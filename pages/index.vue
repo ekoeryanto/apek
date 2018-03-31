f288551a8d5e5fa3eb067885668f4aaa9054aa6f
Eko Eryanto <ekoeryanto@gmail.com>
2018-03-31 13:14:35 +0700

    chore: use carousel

--- pages/index.vue
+++ pages/index.vue
@@ -5,55 +5,64 @@
       fill-height
       class="px-0 py-0"
     >
-      <img width="100%" :src="jumbotron.background" alt="headline image">
+      <v-carousel v-if="$data.carousel" :hide-controls="carousel.length < 2" :hide-delimiters="carousel.length < 2">
+        <v-carousel-item v-for="(item, i) in carousel" :src="item.image" :key="i"></v-carousel-item>
+      </v-carousel>
     </v-container>
     <div
+      v-if="$data.business"
       class="business-field bgx"
     >
       <v-container
         class="text-xs-center"
         grid-list-xl
       >
-        <h2 class="display-1 mb-4 white--text">Business Fields</h2>
-        <v-layout
-          row
-          wrap
-          justify-center
-        >
-          <v-flex
-            v-for="b in business"
-            :key="b.title"
-            md3
-            class="my-3"
+        <template v-if="$data.business">
+          <h2 class="display-1 mb-4 white--text">Business Fields</h2>
+          <v-layout
+            row
+            wrap
+            justify-center
           >
-            <v-icon
-              dark
-              size="100px"
-              v-text="b.icon"
-            />
-            <div class="title my-4 white--text">{{ b.title }}</div>
-          </v-flex>
-        </v-layout>
-        <h2 class="mt-5 pt-5 display-1 mb-4 white--text">Sponsors</h2>
-        <v-layout
-          row
-          wrap
-          justify-center
-        >
-          <v-flex
-            v-for="b in sponsor"
-            :key="b.title"
-            md3
-            class="my-3"
-          >
-            <img
-              :src="b.image"
-              :alt="b.title"
-              height="80px"
+            <v-flex
+              v-for="b in business"
+              :key="b.title"
+              md3
+              class="my-3"
             >
-            <!-- <div class="title my-4 white--text">{{ b.title }}</div> -->
-          </v-flex>
-        </v-layout>
+              <v-icon
+                dark
+                size="100px"
+                v-text="b.icon"
+              />
+              <div class="title my-4 white--text">{{ b.title }}</div>
+            </v-flex>
+          </v-layout>
+        </template>
+
+        <template v-if="$data.sponsor">
+          <h2 class="mt-5 pt-5 display-1 mb-4 white--text">Sponsors</h2>
+          <v-layout
+            row
+            wrap
+            justify-center
+          >
+            <v-flex
+              v-for="b in sponsor"
+              :key="b.title"
+              md3
+              class="my-3"
+            >
+              <img
+                :src="b.image"
+                :alt="b.title"
+                :title="b.title"
+                height="80px"
+              >
+              <!-- <div class="title my-4 white--text">{{ b.title }}</div> -->
+            </v-flex>
+          </v-layout>
+        </template>
       </v-container>
     </div>
   </div>
@@ -62,11 +71,15 @@
 <script>
 import ProgressiveBackground from '@/components/background.vue';
 import ProgressiveImage from '@/components/image.vue';
+import VCarousel from 'vuetify/es5/components/VCarousel/VCarousel';
+import VCarouselItem from 'vuetify/es5/components/VCarousel/VCarouselItem';
 
 export default {
   components: {
     ProgressiveBackground,
     ProgressiveImage,
+    VCarousel,
+    VCarouselItem,
   },
   head() {
     return {
@@ -123,5 +136,9 @@ export default {
 
 .vue-typer .custom.caret {
   display: none;
+}
+.jumbotron__image {
+  width: 100%;
+  height: 100%;
 }
 </style>
