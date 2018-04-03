44fec1844284393c3acb65e2daf8594f7d6e4f7c
Eko Eryanto <ekoeryanto@gmail.com>
2018-04-04 00:47:28 +0700

    chore: add blog pagination

--- pages/activities.vue
+++ pages/activities.vue
@@ -1,18 +1,34 @@
 <template>
-<section class="container">
-    <h2>Activities</h2>
-    <ul>
-      <li v-for="post in posts" :key="post.date">
-        <nuxt-link :to="post._path">
-          {{ post.title }}
-        </nuxt-link>
-      </li>
-    </ul>
-  </section>
+  <v-container grid-list-md>
+    <h1>Activities</h1>
+    <v-list three-line>
+      <v-data-iterator
+        :items="posts"
+      >
+        <template  slot="item" slot-scope="props">
+          <v-list-tile avatar :to="props.item._path">
+            <v-list-tile-avatar>
+              <img :src="props.item.thumbnail">
+            </v-list-tile-avatar>
+            <v-list-tile-content>
+              <v-list-tile-title v-html="props.item.title"></v-list-tile-title>
+              <v-list-tile-sub-title v-html="props.item.description || props.item.excerpt || props.item.body"></v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </template>
+      </v-data-iterator>
+    </v-list>
+  </v-container>
 </template>
 
 <script>
+import { VDataIterator, VDivider } from 'vuetify';
+
 export default {
+  components: {
+    VDataIterator,
+    VDivider,
+  },
   data() {
     const context = require.context(
       '~/content/blog/posts/activities/',
