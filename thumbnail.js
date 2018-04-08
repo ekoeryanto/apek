3c75229b6087ba70673df0111bba83899a5342a7
Eko Eryanto <ekoeryanto@gmail.com>
2018-04-09 05:20:17 +0700

    chore: selective smallize the image

--- thumbnail.js
+++ thumbnail.js
@@ -1,23 +1,28 @@
-const path = require('path')
-const sharp = require('sharp')
+const path = require('path');
+const sharp = require('sharp');
+const sizeImg = require('image-size');
 
-const args = process.argv.slice(2).map(p => path.resolve(p))
+const args = process.argv.slice(2).map(p => path.resolve(p));
 
 args.forEach(file => {
-  const thumbPath = toThumbPath(file)
-  console.log(`creating tumbnail for ${file}`)
-
-  sharp(file)
-    .resize(30)
-    .toFile(thumbPath)
-    .catch(e => console.error(`can't process ${file}`, e))
-    .then(a => {
-      console.log(`tumbnail created in ${thumbPath}`)
-    })
-})
+  sizeImg(file, (err, dimension) => {
+    if (err) return console.log('skip:', err.message);
+    if (dimension.height > 500) {
+      const thumbPath = toThumbPath(file);
+      console.log(`creating tumbnail for ${file}`);
+      sharp(file)
+        .resize(30)
+        .toFile(thumbPath)
+        .catch(e => console.error(`can't process ${file}`, e))
+        .then(a => {
+          console.log(`tumbnail created in ${thumbPath}`);
+        });
+    }
+  });
+});
 
 function toThumbPath(src, dir = 'small') {
-  const ps = src.split(path.sep)
-  ps.splice(ps.length - 1, 0, 'small')
-  return ps.join(path.sep)
+  const ps = src.split(path.sep);
+  ps.splice(ps.length - 1, 0, 'small');
+  return path.join(...ps);
 }
