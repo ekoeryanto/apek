dea07840e880438aa9999fa5931873e138c0bc92
Eko Eryanto <ekoeryanto@gmail.com>
2018-04-09 20:07:11 +0700

    chore: clean up code

--- pages/products.vue
+++ pages/products.vue
@@ -4,9 +4,6 @@
     <v-container
       v-if="$data.body"
       v-html="$marked($data.body)"/>
-    <v-container v-else>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et id sunt facilis tempora eum eaque saepe delectus corrupti eius rerum harum magni, vitae soluta error molestias, doloribus ipsam? Voluptatem, amet.
-    </v-container>
   </div>
 </template>
 
