10a2235d2c2e625d434bec425acf67bc827c93e8
Eko Eryanto <ekoeryanto@gmail.com>
2018-04-09 16:51:56 +0700

    chore: stretch structure org. image

--- pages/structure.vue
+++ pages/structure.vue
@@ -1,11 +1,9 @@
 <template>
   <div>
     <apek-title :title="title"/>
-    <v-container
-      v-if="$data.body"
-      v-html="$marked($data.body)"/>
-    <v-container v-else>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et id sunt facilis tempora eum eaque saepe delectus corrupti eius rerum harum magni, vitae soluta error molestias, doloribus ipsam? Voluptatem, amet.
+    <v-container>
+      <img :src="$data.image" alt="Struktur Organisasi" width="100%">
+      <div v-if="$data.body" v-html="$marked($data.body)" />
     </v-container>
   </div>
 </template>
