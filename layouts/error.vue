8250180b1bffba75425a44b2131f85c6a90e0240
Eko Eryanto <ekoeryanto@gmail.com>
2018-04-11 13:34:26 +0700

    feat: add error page

---
+++ layouts/error.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container fill-height>
+    <v-layout align-center>
+      <v-flex class="text-xs-center">
+        <h1 class="display-2 primary--text">
+          Whoops, {{ error.statusCode }}
+        </h1>
+        <p class="subtitle my-4">
+          {{ error.message }}
+        </p>
+        <v-btn outline color="accent" to="/" replace>
+          Get me out of here!
+        </v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+};
+</script>
