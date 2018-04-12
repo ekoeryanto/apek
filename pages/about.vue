6112ceb8415e79644da4d8e461973a532b36c133
Eko Eryanto <ekoeryanto@gmail.com>
2018-04-12 12:02:30 +0700

    chore: add about page

---
+++ pages/about.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <apek-title :title="title"/>
+    <v-container v-if="$data.body" v-html="$marked($data.body)" class="markdown-body body-1 color-inherit" />
+  </div>
+</template>
+
+<script>
+import ApekTitle from '~/components/ApekTitle';
+
+export default {
+  components: {
+    ApekTitle,
+  },
+  head() {
+    return {
+      title: this.title,
+    };
+  },
+  data: () => ({}),
+  asyncData() {
+    return import('~/content/pages/about.json');
+  },
+};
+</script>
