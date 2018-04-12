8bfb0053873d4bbd1a490776f8a936658b1bdf23
Eko Eryanto <ekoeryanto@gmail.com>
2018-04-12 08:57:57 +0700

    chore: safely using async data

--- pages/member/_member.vue
+++ pages/member/_member.vue
@@ -4,7 +4,7 @@
     <v-container grid-list-md>
       <v-layout row wrap>
         <v-flex xs12 sm12 md10>
-          <img :src="thumbnail" :alt="title" width="100%">
+          <img v-if="$data.thumbnail" :src="$data.thumbnail" :alt="title" width="100%">
         </v-flex>
         <v-flex v-if="$data.detail" v-html="$marked($data.detail)" class="markdown-body body-1 color-inherit" />
       </v-layout>
