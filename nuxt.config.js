566f7e0e0eb0688a1052cde21a2f814c861155ce
Eko Eryanto <ekoeryanto@gmail.com>
2018-04-15 23:40:27 +0700

    chore: update sitemap

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,42 +1,40 @@
 const glob = require('glob');
+const fs = require('fs');
 const path = require('path');
-const pkg = require('./package')
+const pkg = require('./package');
 const _ = require('lodash');
 
-const nodeExternals = require('webpack-node-externals')
-let publicURL = 'http://localhost:3000'
+const nodeExternals = require('webpack-node-externals');
+let publicURL = 'http://localhost:3000';
 
 switch (process.env.CONTEXT) {
   case 'production':
-    publicURL = process.env.URL
+    publicURL = process.env.URL;
     break;
   case 'branch-deploy':
-    publicURL = process.env.URL.replace('://', `://${process.env.BRANCH}.`)
+    publicURL = process.env.URL.replace('://', `://${process.env.BRANCH}.`);
     break;
   case 'deploy-preview':
-    publicURL = process.env.DEPLOY_PRIME_URL
+    publicURL = process.env.DEPLOY_PRIME_URL;
     break;
 }
 
-const dynamicRoutes = getDynamicPaths({
+const routes = {
   '/announcement': 'blog/posts/announcements/*.json',
   '/activity': 'blog/posts/activities/*.json',
   '/technology': 'blog/posts/technologies/*.json',
-  '/member': 'members/*.json'
-});
+  '/member': 'members/*.json',
+};
 
 const brandIcons = _.uniq(
-  require('./content/pages/contact.json')
-  .social
-    .map(i => _.camelCase(`fa ${i.icon[1]}`))
-)
+  require('./content/pages/contact.json').social.map(i =>
+    _.camelCase(`fa ${i.icon[1]}`)
+  )
+);
 
 const solidIcons = require('./content/pages/home.json')
-  .business
-  .map(i => _.camelCase(`fa ${i.icon[1]}`))
-  .concat(['faFax', 'faPhone', 'faBuilding', 'faEnvelope', 'faIndustry'])
-
-
+  .business.map(i => _.camelCase(`fa ${i.icon[1]}`))
+  .concat(['faFax', 'faPhone', 'faBuilding', 'faEnvelope', 'faIndustry']);
 
 module.exports = {
   mode: 'universal',
@@ -51,13 +49,21 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: pkg.description },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: "stylesheet", href: "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons"},
-      { rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.10.0/github-markdown.min.css"},
-    ]
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons',
+      },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.10.0/github-markdown.min.css',
+      },
+    ],
   },
 
   /*
@@ -74,23 +80,18 @@ module.exports = {
   */
   generate: {
     fallback: true,
-    routes: dynamicRoutes
+    routes: getDynamicPaths(routes),
   },
 
   /*
   ** Global CSS
   */
-  css: [
-    '@/assets/style/app.styl'
-  ],
+  css: ['@/assets/style/app.styl'],
 
   /*
   ** Plugins to load before mounting the App
   */
-  plugins: [
-    '@/plugins/prototypes',
-    '@/plugins/vuetify',
-  ],
+  plugins: ['@/plugins/prototypes', '@/plugins/vuetify'],
 
   /*
   ** Nuxt.js modules
@@ -99,7 +100,7 @@ module.exports = {
     'nuxt-fontawesome',
     '@nuxtjs/pwa',
     '@nuxtjs/google-analytics',
-    '@nuxtjs/sitemap'
+    '@nuxtjs/sitemap',
   ],
 
   /*
@@ -110,43 +111,43 @@ module.exports = {
     imports: [
       {
         set: '@fortawesome/fontawesome-free-brands',
-        icons: _.uniq(brandIcons)
+        icons: _.uniq(brandIcons),
       },
       {
         set: '@fortawesome/fontawesome-free-solid',
-        icons: _.uniq(solidIcons)
-      }
-    ]
+        icons: _.uniq(solidIcons),
+      },
+    ],
   },
-  icon: { iconSrc: 'static/images/icon.png'},
+  icon: { iconSrc: 'static/images/icon.png' },
   workbox: {
     runtimeCaching: [
       {
         urlPattern: 'https://fonts.(googleapis|gstatics).com/.*',
-        handler: 'cacheFirst'
+        handler: 'cacheFirst',
       },
       {
         urlPattern: 'https://cdnjs.cloudflare.com/.*',
-        handler: 'cacheFirst'
+        handler: 'cacheFirst',
       },
       {
         urlPattern: 'https://unpkg.com/.*',
-        handler: 'cacheFirst'
-      }
-    ]
+        handler: 'cacheFirst',
+      },
+    ],
   },
   meta: {
     name: pkg.name.toUpperCase(),
     nativeUI: true,
-    ogHost: publicURL
+    ogHost: publicURL,
   },
   sitemap: {
     generate: true,
     hostname: publicURL,
-    routes: dynamicRoutes
+    routes: getDynamicPaths(routes, 'content', true),
   },
   'google-analytics': {
-    id: 'UA-114389898-1'
+    id: 'UA-114389898-1',
   },
 
   /*
@@ -161,11 +162,11 @@ module.exports = {
           {
             vuetify: {
               transform: 'vuetify/es5/components/${member}',
-              preventFullImport: true
-            }
-          }
-        ]
-      ]
+              preventFullImport: true,
+            },
+          },
+        ],
+      ],
     },
     vendor: ['~/plugins/vuetify.js'],
     extractCSS: true,
@@ -175,8 +176,10 @@ module.exports = {
     ** You can extend webpack config here
     */
     extend(config, ctx) {
-      config.resolve.alias['@fortawesome/fontawesome-free-brands$'] = '@fortawesome/fontawesome-free-brands/shakable.es.js'
-      config.resolve.alias['@fortawesome/fontawesome-free-solid$'] = '@fortawesome/fontawesome-free-solid/shakable.es.js'
+      config.resolve.alias['@fortawesome/fontawesome-free-brands$'] =
+        '@fortawesome/fontawesome-free-brands/shakable.es.js';
+      config.resolve.alias['@fortawesome/fontawesome-free-solid$'] =
+        '@fortawesome/fontawesome-free-solid/shakable.es.js';
       if (ctx.isDev) {
         config.module.rules.push({
           test: /\.(gif|jpe?g|png|svg|tiff|webp)$/,
@@ -186,48 +189,57 @@ module.exports = {
               options: {
                 jpeg: {
                   progressive: true,
-                  blur: true
+                  blur: true,
                 },
                 png: {
                   progressive: true,
-                  blur: true
-                }
-              }
-            }
-          ]
-        })
+                  blur: true,
+                },
+              },
+            },
+          ],
+        });
         // Run ESLint on save
         if (ctx.isClient) {
           config.module.rules.push({
             enforce: 'pre',
             test: /\.(js|vue)$/,
             loader: 'eslint-loader',
-            exclude: /(node_modules)/
-          })
+            exclude: /(node_modules)/,
+          });
         }
       }
       if (ctx.isServer) {
         config.externals = [
           nodeExternals({
-            whitelist: [/^vuetify/]
-          })
-        ]
+            whitelist: [/^vuetify/],
+          }),
+        ];
       }
-    }
-  }
-}
+    },
+  },
+};
 
 /**
  * Create an array of URLs from a list of files
  * @param {*} urlFilepathTable
  */
-function getDynamicPaths(urlFilepathTable, cwd = 'content') {
+function getDynamicPaths(urlFilepathTable, cwd = 'content', sitemap = false) {
   return [].concat(
-    ...Object.keys(urlFilepathTable).map(url => {
-      const filepathGlob = urlFilepathTable[url];
-      return glob
-        .sync(filepathGlob, { cwd })
-        .map(filepath => `${url}/${path.basename(filepath, '.json')}`);
+    ...Object.keys(urlFilepathTable).map(uri => {
+      const filepathGlob = urlFilepathTable[uri];
+      return glob.sync(filepathGlob, { cwd }).map(filepath => {
+        const url = `${uri}/${path.basename(filepath, '.json')}`;
+        if (sitemap) {
+          return {
+            lastmodISO: fs.statSync(path.join(cwd, filepath)).mtime,
+            changefreq: 'daily',
+            url,
+          };
+        }
+
+        return url;
+      });
     })
   );
 }
