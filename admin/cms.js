43e9afb0ec7c0c4ec22260318ca318fc277fc5ff
Eko Eryanto <ekoeryanto@gmail.com>
2018-04-20 04:27:44 +0700

    chore(admin): implement color widget

--- admin/cms.js
+++ admin/cms.js
@@ -5,9 +5,11 @@ import 'netlify-cms/dist/cms.css';
 import { youtube } from './editors';
 import * as Material from './widgets/material-design-icon';
 import * as fa from './widgets/fontawesome';
+import * as color from './widgets/color';
 
 window.CMS = CMS;
 
 CMS.registerEditorComponent(youtube);
 CMS.registerWidget('icon', Material.Control);
 CMS.registerWidget('fontawesome', fa.Control);
+CMS.registerWidget('color', color.Control);
